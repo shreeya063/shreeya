1b3652bb3fce1c4a837a08a6cbd729071dfc3f91
shreeya063 <shreeyap@metropolia.fi>
2024-08-22 10:13:58 +0300

    Add files via upload

---
+++ Module2_question4.py
@@ -0,0 +1,8 @@
+int_1 = int(input("Enter the first integer number: "))
+int_2 = int(input("Enter the second integer number: "))
+int_3 = int(input("Enter the third integer number: "))
+
+sum = int_1 + int_2 + int_3
+product = int_1 * int_2 * int_3
+
+print("The sum of the numbers is " + str(sum) + " , the product is " + str(product) + " and the average is " + str(sum/3))
---
+++ module2_question1.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+
+
+
+
+
+
+
+
